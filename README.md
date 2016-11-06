997e51d4b0a970a2ce648c90fc8a17c6117db7b6
yoavdamri <yoavdamri@gmail.com>
2016-11-06 11:46:46 +0200

    first ng2 component

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # D3v4Exanples
+This project will start with NG2
---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'first-component',
+    template: '<h1>this is my first ng2 component</h1>'
+})
+export class AppComponent {}
